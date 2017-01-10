eceff7b078a43fac2154ffe3bd69c24de32475b3
Rahul Raja <rahul.raja@inmobi.com>
2017-01-10 19:54:49 +0530

    Way Too Easy

---
+++ Forces/WAYTLW.cpp
@@ -0,0 +1,13 @@
+http://codeforces.com/problemset/problem/71/A
+
+#include<iostream>
+using namespace std;
+int n,i,k,a;
+string s;
+main()
+{cin>>n;
+for(i=0;i<n;i++)
+{cin>>s;
+k=s.size();
+if(k<=10)cout<<s<<endl;
+else cout<<s[0]<<k-2<<s[k-1]<<endl;}}
