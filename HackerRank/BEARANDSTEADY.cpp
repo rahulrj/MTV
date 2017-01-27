77e99c1270040df10c47e31c9143f18e2af718d5
Rahul Raja <rahul.raja@inmobi.com>
2017-01-27 23:15:07 +0530

    Two pointers

--- HackerRank/BEARANDSTEADY.cpp
+++ HackerRank/BEARANDSTEADY.cpp
@@ -1,41 +1,39 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-
-
+typedef long long ll;
+ll n,freq[4];
+string s,ch = "ATCG";
+ll pos(ll j){
+    for(ll i=0;i<4;i++){
+        if(s[j] == ch[i]) return i;
+    }
+    return 0;
+}
+bool is_ok(){
+    for(int i=0;i<4;i++){
+        if(freq[i] > n/4) return false;
+    }
+    return true;
+}
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    int n;
-    string s;
-    cin>>s;
-    int freq[26]={0};
-    int steady=n/4;
-    int sum=0;
-
-    for(int i=0;i<n;i++){
-        freq[s[i]]++;
+    cin>>n>>s;
+    memset(freq,0,sizeof(freq));
+    for(ll i=0;i<n;i++){
+        ++freq[pos(i)];
+    } 
+    if(is_ok()) {
+        cout<<0<<endl;
+        return 0;
     }
-
-    freq['A']=freq['A']-steady; sum+=freq['A']>0?freq['A']:0;
-    freq['T']=freq['T']-steady; sum+=freq['T']>0?freq['T']:0;
-    freq['G']=freq['G']-steady; sum+=freq['G']>0?freq['G']:0;
-    freq['C']=freq['C']-steady; sum+=freq['C']>0?freq['C']:0;
-
-    for(int i=0;i<n;i++){
-        string sub="";
-        for(int j=sum;j<=n;j++){
-            if(sub!=""){
-                sub+=s[j];
-            }
-            else{
-                sub=s.substr(i,j);
-            }
+    ll ans = n,j = 0;
+    for(ll i=0;i<n;i++){
+        while(j<n&&!is_ok()){
+            --freq[pos(j++)];
         }
+        if(is_ok()) ans = min(ans,j-i) ;
+        ++freq[pos(i)];
     }
-
-
+    cout<<ans<<endl;
     return 0;
 }
