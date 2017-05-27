808dfc6a9cb20e21f227f165e185e575cb7f58e5
Rahul Raja <rahul.raja@inmobi.com>
2017-05-27 21:01:03 +0530

    Max Distance Optimized solution

--- InBit/Arrays/MaxDistance.cpp
+++ InBit/Arrays/MaxDistance.cpp
@@ -1,38 +1,17 @@
-https://www.interviewbit.com/problems/max-distance/
-
-int Solution::maximumGap(const vector<int> &A) {
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    vector<bool> tempArr(A.size());
-    int min = A[0];
-    int max_distance = 0;
-    int i = A.size()-1;
-    int j = A.size()-1;
-
-    for(int i = 0; i < A.size(); i++){
-        if(A[i] > min){
-            tempArr[i] = false;
-        }
-        else{
-            min = A[i];
-            tempArr[i] = true;
-        }
-    }
-
-    LOOP:while(i >= 0){
-        if(tempArr[i] == false){
-            i--;
-            goto LOOP;
-        }
-        while((A[i] > A[j]) && (j > i)){
-            j--;
-        }
-        if((j-i) > max_distance){
-            max_distance = j-i;
-        }
-        i--;
-    }
-    return max_distance;
-}
+int maximumGap(const vector<int> &num) {
+           if (num.size() == 0) return -1;
+           if (num.size() == 1) return 0;
+           vector<pair<int, int> > toSort;
+           for (int i = 0; i < num.size(); i++) {
+               toSort.push_back(make_pair(num[i], i));
+           }
+           sort(toSort.begin(), toSort.end());
+           int len = toSort.size();
+           int maxIndex = toSort[len - 1].second;
+           int ans = 0;
+           for (int i = len - 2; i >= 0; i--) {
+               ans = max(ans, maxIndex - toSort[i].second);
+               maxIndex = max(maxIndex, toSort[i].second);
+           }
+           return ans;
+       }
