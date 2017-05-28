641c887c049cc0e9000791d9199121556ac45e27
Rahul Raja <rahul.raja@inmobi.com>
2017-05-28 19:52:59 +0530

    Next Permutation

---
+++ InBit/Arrays/NextPermutation.cpp
@@ -0,0 +1,22 @@
+https://www.interviewbit.com/problems/next-permutation/
+class Solution {
+    public:
+        void nextPermutation(vector<int> &num) {
+            int len = num.size();
+            int i,j;
+            for(i = len - 2; i >= 0 ; i--)
+                if(num[i] < num[i+1]) break;
+
+            if (i == -1) {
+                reverse(num.begin(), num.end());
+                return;
+            }
+
+            for(j = len - 1; j > i; j--)
+                if(num[j] > num[i]) break;
+
+            swap(num[i], num[j]);
+            reverse(num.begin() + i + 1, num.end());
+            return;
+        }
+};
