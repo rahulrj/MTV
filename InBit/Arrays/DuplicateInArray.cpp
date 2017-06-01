a56ad5426218640443639ced81d2797bc510bec5
Rahul Raja <rahul.raja@inmobi.com>
2017-06-01 19:26:51 +0530

    Duplicate in Array optimized

--- InBit/Arrays/DuplicateInArray.cpp
+++ InBit/Arrays/DuplicateInArray.cpp
@@ -1,64 +1,39 @@
-https://www.interviewbit.com/problems/find-duplicate-in-array/
+int Solution::repeatedNumber(const vector<int> &A) {
 
-public class Solution {
-	// DO NOT MODIFY THE LIST
-	private int[] buckets;
+    const int n=A.size();
+    const int aSize=n/2;
+    std::vector<int> arr(n/2);
+    //int *arr=new int[n/2];
+    //std::fill(arr, arr+(n/2), 0);
 
-    private int AVG_BUCKET_SIZE = 0;
-
-    private int LAST_BUCKET_SIZE = 0;
-
-    // DO NOT MODIFY THE LIST
-    public int repeatedNumber(final List<Integer> A) {
-        int n = A.size() - 1; // [1 : n] for (n + 1) number
-        if (n < 1) {
-            // no way to find duplicates
-            return -1;
-        }
-        int nBuckets = (int) Math.ceil(Math.sqrt(n)); // ceiling of sqrt(n)
-        int range = (int) Math.sqrt(n); // floor of sqrt(n)
-
-        buckets = new int[nBuckets];
-        if (range * nBuckets < n) {
-            // enlarge range of each bucket to contain all elements
-            range++;
+    for(int i=0;i<n;i++){
+        int num=A[i];
+        int originalNum=num;
+        if(num>n/2){
+            num=num-n/2;
         }
 
-        LAST_BUCKET_SIZE = n - (nBuckets - 1) * range;
 
-        AVG_BUCKET_SIZE = range;
+        int index=num-1;
+        if(arr[index]==0){
+            arr[index]=originalNum;
+        }
+        else{
 
-        int targetBucket = -1;
-        for (int num : A) {
-            int iBucket = (num - 1) / range;
-            buckets[iBucket]++;
-            if (moreThanExpected(iBucket)) {
-                // once we found a bucket holding more elements than expected
-                targetBucket = iBucket;
-                break;
+            if(arr[index]==originalNum){
+                //cout<<"H1"<<originalNum<<" "<<arr[index];
+                return originalNum;
             }
-        }
-        if (targetBucket != -1) {
-            Set<Integer> targetSet = new HashSet<>();
-            int lower = targetBucket * range + 1;
-            int upper = lower + range - 1;
-            for (int num : A) {
-                if (num >= lower && num <= upper && !targetSet.add(num)) {
-                    // duplicate found
-                    return num;
-                }
+            else if(arr[index]<0){
+                //cout<<"H2"<<originalNum<<" "<<arr[index];
+                return originalNum;
             }
-        }
-        return -1;
-    }
+            else{
+                arr[index]=-1;
+            }
 
-    // whether bucket[n] has more than expected elements
-    private boolean moreThanExpected(int nBucket) {
-        if (nBucket == buckets.length - 1) {
-            return buckets[nBucket] > LAST_BUCKET_SIZE;
-        } else {
-            return buckets[nBucket] > AVG_BUCKET_SIZE;
         }
     }
 
+    return -1;
 }
