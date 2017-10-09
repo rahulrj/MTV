62a158afb80a0fd8983476653d2f7dfd078caa13
Rahul Raja <rahul.110392@gmail.com>
2017-10-09 14:21:28 -0400

    3 sum genius solution

--- LeetCode/PHONE_COMBINATIONS.java
+++ LeetCode/PHONE_COMBINATIONS.java
@@ -31,3 +31,29 @@ class Solution {
         
     }
 }
+
+
+// Other solution by a genius
+class Solution {
+    
+    public List<String> letterCombinations(String digits) {
+    LinkedList<String> ans = new LinkedList<String>();
+    String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    ans.add("");
+        
+        
+    for(int i =0; i<digits.length();i++){
+        int x = Character.getNumericValue(digits.charAt(i));
+        System.out.println(""+x);
+         System.out.println(ans);
+        System.out.println(ans.peek());
+        while(ans.peek().length()==i){
+            String t = ans.remove();
+            for(char s : mapping[x].toCharArray())
+                ans.add(t+s);
+            System.out.println("ans"+ans);
+        }
+    }
+    return ans;
+}
+}
